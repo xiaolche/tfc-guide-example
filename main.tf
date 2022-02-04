25ba19329e3f1c08c046ef88ae5e479ccae9d48d
xiaolche <46457668+xiaolche@users.noreply.github.com>
2022-02-04 11:48:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "ami-0a8b4cd432b1c3063"
   instance_type = var.instance_type
-
+  vpc_security_group_ids = ["sg-0a5ec3465edf6ff9a"]
+  subnet_id              = "subnet-09197f1e450cc54fc"
   tags = {
     Name = var.instance_name
   }
